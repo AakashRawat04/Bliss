dbdceb9aa3f3bf76c5454a513ea36c1e59a2c62a
Ash <01tripster@gmail.com>
2022-08-19 20:35:46 +0530

    sample commands with if/else if code block

--- index.js
+++ index.js
@@ -10,5 +10,21 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('pong!');
+	}
+	else if (commandName === 'server') {
+		await interaction.reply(`**Server Name:** ${interaction.guild.name}\n**Total members:** ${interaction.guild.memberCount}\n**Server creation date:** ${interaction.guild.createdAt}\n**Server Verification Level:** ${interaction.guild.verificationLevel}`);
+	}
+	else if (commandName === 'user') {
+		await interaction.reply(`User tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
 // Login to Discord with your client's token
 client.login(token);
